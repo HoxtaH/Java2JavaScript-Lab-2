dbf9382860d9d8a996281176749a527b16956532
HoxtaH <tyrellhoxter@gmail.com>
2016-05-30 20:33:25 -0400

    first commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,56 @@
 " use strict ";
 
+class Pet {
+  constructor(type,name){
+      this.name = name;
+      this.type = type;
+}
+  speak(){
+    display.innerHTML += "Pet Overlord ";
+    }
+}
+class Dog extends Pet{
+   constructor(name){
+    super(Dog,name);
+   }
+
+   speak(){
+    display.innerHTML += "Bark..Bark WOOF! ";
+   }
+}
+
+class Cat extends Pet{
+  constructor(name){
+    super(Cat, name);
+  }
+
+  speak(){
+    display.innerHTML += "Meow..me..PRRRRR! ";
+  }
+}
+
+class Bird extends Pet{
+  constructor(name){
+    super(Bird,name);
+  }
+
+  speak(){
+    display.innerHTML += "Chirp chirp.. TWEET! ";
+  }
+
+}
+
+class RickRoss extends Pet{
+  constructor(name){
+    super(RickRoss,name);
+  }
+
+  speak(){
+    display.innerHTML += "ahyuhhhgghh... BOSSSSSSS! ";
+  }
+}
+
+
 function PetChat(){
   var display = document.getElementById("display");
   var numPets = null;
@@ -10,9 +61,22 @@ function PetChat(){
 
   var askPetInfo = function(){
     for(var i=0; i<numPets; i++){
-      var type = prompt("What is pet #" + (i+1) + "? (cat, dog, bird)");
+      var type = prompt("What is pet #" + (i+1) + "? (cat, dog, bird, rickross)");
       var name = prompt("What is pet #" + (i+1) + "'s name'?");
-      var textOut = type + " " + name + "<br />";
+      if(type.toLowerCase() == "dog"){
+      var d =  new Dog(name,type).speak();
+      }
+      if(type.toLowerCase() == "cat"){
+       var c = new Cat(name, type).speak();
+      }
+      if(type.toLowerCase() == "bird"){
+       var b = new Bird(name,type).speak();
+      }
+      if(type.toLowerCase() == "rickross"){
+       var r = new RickRoss(name,type).speak();
+      }
+
+      var textOut = " " + type + " " + name + " " +  "<br />";
       display.innerHTML += textOut;
     }
   };
@@ -25,6 +89,9 @@ function PetChat(){
     askPetInfo();
   }
 };
+
+
+
 
 var chat = new PetChat();
 chat.init();
